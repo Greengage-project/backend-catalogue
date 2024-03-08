815c4d185e394cfbcdd894329b6a7e1a395b4698
Rubén Sánchez <ruben.sanchez@deusto.es>
2024-03-08 10:07:48 +0100

    Test changes on problem profiles and languages

--- catalogue/seed/configuration.py
+++ catalogue/seed/configuration.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
+
 class Environments(Enum):
     varam = "varam"
     mef = "mef"
     zgz = "zgz"
+
 
 class Languages(Enum):
     en = "en"
@@ -11,4 +13,7 @@ class Languages(Enum):
     lv = "lv"
     it = "it"
     nl = "nl"
+    dk = "dk"
+
+
 lang_keys = [e.value for e in Languages]
